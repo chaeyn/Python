37d40a7458b674dc806b80f74ca73e0998ea8943
chaeyn <chaulfe@gmail.com>
2024-12-10 23:05:58 +0900

    push

---
+++ bikoweek/num1.py
@@ -0,0 +1,5 @@
+a, r = input().split()
+a = float(a)
+r = int(r)
+res = a*r
+print("{:.6f}".format(res))
---
+++ boj/11557.py
@@ -0,0 +1,9 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    for j in range(n):
+        min = int(0)
+        s, l = input().split()
+        if l<=min:
+            min = l
+        

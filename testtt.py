2052244e57d1db7425805713cdf3200f13e34bcb
chaeyn <chaulfe@gmail.com>
2025-02-03 14:59:32 +0900

    commit

---
+++ testtt.py
@@ -0,0 +1,5 @@
+pocket = ['paper', 'n', 'phon']
+if 'money' in pocket:
+    pass
+else:
+    print("카드를 꺼내라")
---
+++ testwhile.py
@@ -0,0 +1,6 @@
+even = []
+num = 1
+while num < 10:
+	even.append(num)
+	num += 2
+print(even)

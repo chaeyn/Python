596d8c3b93124d1d627e6c5cba7453b13686a212
chaeyn <chaulfe@gmail.com>
2025-02-05 14:39:03 +0900

    commit

---
+++ list1.py
@@ -0,0 +1,9 @@
+odd = [1, 3, 5, 7, 9]
+cafes = ['star', 'bene', 'yoger', 'friends']
+A = [1, 5, 'A', 'CC', 'B']
+listInList = [[1, 3, 5, 6, 7], cafes, odd, 1, 3, 'Abc']
+
+print(odd)
+print(cafes)
+print(A)
+print(listInList)

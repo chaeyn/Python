10db484137709cb07d205ac7537029d7f2612761
채근영 <chaulfe@gmail.com>
2025-05-04 11:46:07 +0900

    update

--- boj/11557.py
+++ boj/11557.py
@@ -1,9 +1,11 @@
 t = int(input())
 for i in range(t):
     n = int(input())
+    lst_n = []
+    lst_l = []
     for j in range(n):
-        min = int(0)
-        s, l = input().split()
-        if l<=min:
-            min = l
-        
+        n, l = input().split()
+        lst_n.append(n)
+        lst_l.append(int(l))
+    a = lst_l.index(max(lst_l))
+    print(lst_n[a])
